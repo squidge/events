b76b6609e2b8f74b1bfb188bee0b71965763a886
Francisco Ruiz A <frankra@gmail.com>
2013-03-17 12:46:49 +0000

    upgrade to xs part1

--- Events.Web/EventsModule.cs
+++ Events.Web/EventsModule.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Nancy.ModelBinding;
 using ServiceStack.Redis;
 
-namespace Events
+namespace Events.Web
 {
 	public class EventsModule : Nancy.NancyModule
 	{
@@ -13,7 +13,6 @@ namespace Events
 		{
 			Get["/"] = parameters =>
 			{
-				Console.WriteLine("Retrieving all events from Redis storage");
 				var events = redisClient.GetAll<Event>();
 				return View["Index", events];
 			};
@@ -21,7 +20,6 @@ namespace Events
 			Post["/add"] = parameters =>
 			{
 				Event e = this.Bind();
-				Console.WriteLine(String.Format("Saving {0} - {1} in Redis storage", e.What, e.When));
 				redisClient.Store<Event>(e);
 				return e;
 			};
